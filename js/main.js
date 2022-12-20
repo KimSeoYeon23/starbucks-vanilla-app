935640b42fe693bef10f3c70e87827bfad79a1c9
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-20 16:45:29 +0900

    js add

--- js/main.js
+++ js/main.js
@@ -4,8 +4,14 @@ const searchInputEl = searchEl.querySelector('input');
 searchEl.addEventListener('click', function() {
     searchInputEl.focus();
 });
-
+// focus 요소가 포커스를 받을 때
 searchInputEl.addEventListener('focus', function() {
     searchEl.classList.add('focused');
-    searchInputEl.setAttribute();
+    // setAttribute 요소의 속성 값을 정하는 메소드
+    searchInputEl.setAttribute('placeholder', '통합검색');
 });
+// blur 요소가 포커스를 잃을 때
+searchInputEl.addEventListener('blur', function () { 
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+})
