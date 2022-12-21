fd793ea2a8514f4378ee725fd13eebdf19677eff
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-21 13:02:55 +0900

    드롭다운 메뉴

--- js/main.js
+++ js/main.js
@@ -4,14 +4,22 @@ const searchInputEl = searchEl.querySelector('input');
 searchEl.addEventListener('click', function() {
     searchInputEl.focus();
 });
+
 // focus 요소가 포커스를 받을 때
 searchInputEl.addEventListener('focus', function() {
     searchEl.classList.add('focused');
     // setAttribute 요소의 속성 값을 정하는 메소드
     searchInputEl.setAttribute('placeholder', '통합검색');
 });
+
 // blur 요소가 포커스를 잃을 때
 searchInputEl.addEventListener('blur', function () { 
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', function() {
+    console.log('scroll');
 })
