9686b2938e8cb189f36a590718c11791624a1252
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-22 13:01:20 +0900

    promotion slide

--- js/main.js
+++ js/main.js
@@ -55,3 +55,21 @@ new Swiper('.notice-line .swiper-container', {
     autoplay: true,
     loop: true
 });
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3,       // 한 번에 봉줄 슬라이드 개수
+    spaceBetween: 10,       // 슬라이드 사이 여백
+    centeredSlides: true,   // 1번 슬라이드가 가운데 보이기
+    loop: true,             // 슬라이드 반복
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: ".promotion .swiper-pagination",    // 페이지 번호 요소 선택자
+        clickable: true,                        // 상요자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: {
+        nextEl: ".promotion .swiper-next",
+        prevEl: ".promotion .swiper-prev",
+    },
+});
