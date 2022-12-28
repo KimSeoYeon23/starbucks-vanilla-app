59a31e56ce754160951913e106108f54529f2f20
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-28 09:53:48 +0900

    signin page edit

--- js/common.js
+++ js/common.js
@@ -18,6 +18,18 @@ searchInputEl.addEventListener('blur',() => {
     searchInputEl.setAttribute('placeholder', '');
 });
 
+new Swiper('.awards .swiper-container', {
+    slidesPerView: 5,       // 한 번에 봉줄 슬라이드 개수
+    spaceBetween: 30,       // 슬라이드 사이 여백
+    centeredSlides: true,   // 1번 슬라이드가 가운데 보이기
+    loop: true,             // 슬라이드 반복
+    autoplay: true,
+    navigation: {
+        nextEl: ".awards .swiper-next",
+        prevEl: ".awards .swiper-prev",
+    },
+});
+
 // 현재 몇년도인지 갖고옴
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();    // 2022
